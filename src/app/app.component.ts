066f3b3569b8f8da86665b0a251590a6c21c1b41
ralph tashinga nyoni <ralphtashinga@gmail.com>
2018-10-04 22:18:30 +0200

    resolving app home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  template: `
-    <app-gallery [datasource]=images></app-gallery>
-  `,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  images;
-
-  constructor() {
-    this.images = [
-      {'url': 'http://your_image1_url'},
-      {'url': 'http://your_image1_url'}
-    ];
-  }
+  title = 'app';
 }
